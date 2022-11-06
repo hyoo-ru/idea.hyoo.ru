e0689abccf3892175a69a6fa54bdb4c02de1e3aa
jin <jin@hyoo.ru>
2022-11-06 17:02:48 +0800

    + тематический фон

--- app/app.view.css.ts
+++ app/app.view.css.ts
@@ -1,8 +1,14 @@
 namespace $.$$ {
 
 	const { rem } = $mol_style_unit
+	const { url } = $mol_style_func
 
 	$mol_style_define( $hyoo_idea_app, {
+		
+		background: {
+			image: [[ url( 'https://habrastorage.org/webt/ny/6f/4y/ny6f4yk76pjjkxdr30qdasbruum.png' ) ]],
+			size: [ 'cover' ],
+		},
 
 		Menu: {
 			flex: {

bddc55fa98be6de653ae5bbbc2eae647d64cab20
nin-jin <nin-jin@users.noreply.github.com>
2022-11-18 05:51:36 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.test.js
+++ node.test.js
@@ -21322,10 +21322,6 @@ var $;
         const { rem } = $mol_style_unit;
         const { url } = $mol_style_func;
         $mol_style_define($hyoo_idea_app, {
-            background: {
-                image: [[url('https://habrastorage.org/webt/ny/6f/4y/ny6f4yk76pjjkxdr30qdasbruum.png')]],
-                size: ['cover'],
-            },
             Menu: {
                 flex: {
                     basis: rem(15),

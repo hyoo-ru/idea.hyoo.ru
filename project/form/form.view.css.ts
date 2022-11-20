f753e3581caa758ca919df43ce849e8cf3a8e6ea
jin <jin@hyoo.ru>
2022-11-20 18:20:53 +0800

    +1

--- project/form/form.view.css.ts
+++ project/form/form.view.css.ts
@@ -4,15 +4,6 @@ namespace $.$$ {
 
 	$mol_style_define( $.$hyoo_idea_project_form, {
 
-		Logo: {
-			width: rem(5),
-			height: rem(5),
-		},
-
-		Logo_control: {
-			alignItems: 'center',
-		},
-
 		Role_form: $hyoo_idea_person_form_multy,
 		Roles_field: {
 			Label: {

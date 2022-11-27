15c878621bc00e3b4143ec83087ed9d37d47d373
nin-jin <nin-jin@users.noreply.github.com>
2022-11-27 03:45:56 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.esm.js
+++ node.esm.js
@@ -3421,6 +3421,13 @@ var $;
 "use strict";
 var $;
 (function ($) {
+    $.$mol_action = $mol_wire_method;
+})($ || ($ = {}));
+//mol/action/action.ts
+;
+"use strict";
+var $;
+(function ($) {
     class $mol_state_arg extends $mol_object {
         prefix;
         static href(next) {
@@ -3469,6 +3476,9 @@ var $;
             }
             return chunks.join(' ');
         }
+        static go(next) {
+            this.href(this.make_link(next));
+        }
         constructor(prefix = '') {
             super();
             this.prefix = prefix;
@@ -3500,6 +3510,9 @@ var $;
     __decorate([
         $mol_mem_key
     ], $mol_state_arg, "value", null);
+    __decorate([
+        $mol_action
+    ], $mol_state_arg, "go", null);
     $.$mol_state_arg = $mol_state_arg;
 })($ || ($ = {}));
 //mol/state/arg/arg.node.ts
@@ -6048,13 +6061,6 @@ var $;
     $.$mol_wire_sync = $mol_wire_sync;
 })($ || ($ = {}));
 //mol/wire/sync/sync.ts
-;
-"use strict";
-var $;
-(function ($) {
-    $.$mol_action = $mol_wire_method;
-})($ || ($ = {}));
-//mol/action/action.ts
 ;
 "use strict";
 var $;
@@ -14529,7 +14535,7 @@ var $;
             return "async";
         }
         cors() {
-            return "anonymous";
+            return null;
         }
     }
     $.$mol_image = $mol_image;
--- web.d.ts
+++ web.d.ts
@@ -935,6 +935,10 @@ declare namespace $ {
 }
 
 declare namespace $ {
+    let $mol_action: typeof $mol_wire_method;
+}
+
+declare namespace $ {
     class $mol_state_arg extends $mol_object {
         prefix: string;
         static href(next?: string): string;
@@ -957,6 +961,9 @@ declare namespace $ {
         static make_link(next: {
             [key: string]: string | null;
         }): string;
+        static go(next: {
+            [key: string]: string | null;
+        }): void;
         static encode(str: string): string;
         constructor(prefix?: string);
         value(key: string, next?: string): string | null;
@@ -1462,10 +1469,6 @@ declare namespace $ {
         find(include?: RegExp, exclude?: RegExp): $mol_file[];
         size(): number;
     }
-}
-
-declare namespace $ {
-    let $mol_action: typeof $mol_wire_method;
 }
 
 declare namespace $ {
@@ -3918,14 +3921,14 @@ declare namespace $ {
             alt: string;
             loading: string;
             decoding: string;
-            crossOrigin: string;
+            crossOrigin: any;
         };
         minimal_width(): number;
         minimal_height(): number;
         uri(): string;
         loading(): string;
         decoding(): string;
-        cors(): string;
+        cors(): any;
     }
 }
 

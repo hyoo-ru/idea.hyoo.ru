5c22e691b3ef220197af30725499ab3c340f3f7f
jin <jin@hyoo.ru>
2022-11-30 11:48:33 +0800

    Fixed add layout on big message

--- post/add/add.view.css.ts
+++ post/add/add.view.css.ts
@@ -3,8 +3,14 @@ namespace $.$$ {
 	const { per } = $mol_style_unit
 
 	$mol_style_define( $hyoo_idea_post_add, {
+		
+		alignItems: 'flex-end',
 
-		width: per(100),
+		Text: {
+			flex: {
+				shrink: 1,
+			},
+		},
 
 	} )
 

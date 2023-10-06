7fbd189ad8b22dbb37ccfd362329ecc825c49aad
PavelZubkov <PavelZubkov@users.noreply.github.com>
2023-10-06 07:14:00 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.test.js
+++ node.test.js
@@ -5540,6 +5540,9 @@ var $;
                 ]);
                 el.selectionEnd = to;
                 el.selectionStart = from;
+                if (to !== from && el.selectionEnd === el.selectionStart) {
+                    el.selectionEnd = to;
+                }
             }
             selection_start() {
                 const el = this.dom_node();

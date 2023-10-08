d61da8af33aacbc26c1bd122e05842ff70e5392a
PavelZubkov <PavelZubkov@users.noreply.github.com>
2023-10-08 07:13:16 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.d.ts
+++ web.d.ts
@@ -2373,7 +2373,7 @@ declare namespace $ {
 
 declare namespace $ {
     function $mol_wire_race<Tasks extends ((...args: any) => any)[]>(...tasks: Tasks): {
-        [index in keyof Tasks]: index extends number ? ReturnType<Tasks[index]> : Tasks[index];
+        [index in keyof Tasks]: ReturnType<Tasks[index]>;
     };
 }
 
